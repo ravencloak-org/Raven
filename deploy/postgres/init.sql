022e50393317a8467e9ff25851a82c9e370a2773
Jobin Lawrance <jobinlawrance@gmail.com>
2026-04-15 00:22:06 +0530

    feat(infra): replace Zitadel with SuperTokens in dev docker-compose Entire-Checkpoint: 2e305c5ee9b2

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -1,24 +1,6 @@
 -- Raven PostgreSQL initialisation
 -- Runs once when the data volume is first created.
 
--- Zitadel needs its own database + user
-SELECT 'CREATE DATABASE zitadel'
-WHERE NOT EXISTS (SELECT FROM pg_database
-                  WHERE datname = 'zitadel')\gexec
-
--- Default dev password; production deployments should override via
--- ZITADEL_DATABASE_POSTGRES_USER_PASSWORD in docker-compose environment.
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'zitadel') THEN
-    CREATE USER zitadel WITH PASSWORD 'zitadel';
-  END IF;
-END
-$$;
-
-GRANT ALL PRIVILEGES ON DATABASE zitadel TO zitadel;
-ALTER USER zitadel CREATEDB;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "vector";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
